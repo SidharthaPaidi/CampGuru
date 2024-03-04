7a3e5733ca99e1cc9ebe0621e2cc3ba61597d510
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-03-04 09:24:13 +0530

    Add the joi schema validation (running perfectly fine)

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
+const ExpressError = require('./utils/ExpressError')
+const catchAsync = require('./utils/catchAsync')
+const {campgroundSchema} = require('./schemas.js')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 
 mongoose.set('strict', true);
 mongoose.connect('mongodb://127.0.0.1:27017/camp-guru');
-
-
-
-
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -20,12 +19,24 @@ db.once("open", () => {
 
 const app = express()
 
-app.engine('ejs',ejsMate)
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+//middleware for schema validation
+const validateCampground = (req,res,next) => {
+    
+    const {error} = campgroundSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg,400)
+    }else{
+        next()
+    }
+}
 
 //home route
 
@@ -34,10 +45,10 @@ app.get('/', (req, res) => {
 })
 
 //index route
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index', { campgrounds })
-})
+}))
 
 //adding campground route
 app.get('/campgrounds/new', (req, res) => {
@@ -45,55 +56,53 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 //creating campground route
-app.post('/campgrounds', async (req, res, next) => {
-   try {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+    
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
-   }catch(e){
-    next(e)
-   }
-   
-})
+}))
 
 //viewing campground show route
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground })
-})
+}))
 
 //editing campground route
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground })
 
-})
+}))
 
 //updating campground route
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 //deleting campground
-app.delete('/campgrounds/:id', async (req,res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
+}))
+
+
+app.all('*',(req, res, next) => {
+    next(new ExpressError('Page not Found',404))
 })
 
 //error handler
-
-app.use((err,req,res,next) => {
-    res.send("Ohh boii something went wrong")
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
-
-
-
-
-
-app.listen(2000 ,() => {
+app.listen(2000, () => {
     console.log("Listening on Port 2000!!")
 })
