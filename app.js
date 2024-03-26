9d15356abf57cdb1ea112b2d2d9900824abc0516
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-03-26 22:12:21 +0530

    Start Authentication

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const session = require('express-session')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
+
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews')
+const userRoutes = require('./routes/users')
 
 mongoose.set('strict', true);
 mongoose.connect('mongodb://127.0.0.1:27017/camp-guru');
@@ -28,7 +33,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(flash())
+
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
@@ -41,6 +46,14 @@ const sessionConfig = {
     }
 }
 app.use(session(sessionConfig))
+app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
@@ -48,17 +61,16 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
 
 //home route
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home') 
 })
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
 
 //error handler
 app.all('*', (req, res, next) => {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/ExpressError')
 const Campground = require('../models/campground')
 const { campgroundSchema } = require('../schemas.js')
+const { isLoggedIn } = require('../middleware.js')
 
 //middleware for schema validation
 const validateCampground = (req, res, next) => {
@@ -16,7 +17,7 @@ const validateCampground = (req, res, next) => {
         throw new ExpressError(msg, 400)
     } else {
         next()
-    }
+    } 
 }
 
 //index route
@@ -26,22 +27,20 @@ router.get('', catchAsync(async (req, res) => {
 }))
 
 //adding campground route
-router.get('/new', (req, res) => {
+router.get('/new',isLoggedIn, (req, res) => {
     res.render('campgrounds/new')
 })
 
 //creating campground route
-router.post('', validateCampground, catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
-
-    const campground = new Campground(req.body.campground)
+router.post('',isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
+   const campground = new Campground(req.body.campground)
     await campground.save()
     req.flash('success', 'Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 //viewing campground show route
-router.get('/:id', catchAsync(async (req, res) => {
+router.get('/:id',isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews')
     res.render('campgrounds/show', { campground })
 }))
