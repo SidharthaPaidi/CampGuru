d082ced0fca7b0db191a039ce5c9af5bfbbf3b10
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-03-26 16:11:48 +0530

    Add the http.cat image for error handling images

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.all('*', (req, res, next) => {
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err })
+    res.status(statusCode).render('campgrounds/error', {err})
 })
 
 app.listen(2000, () => {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,6 +11,8 @@ const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body)
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
+        res.status(400).render('/views/campgrounds/error', {})
+        
         throw new ExpressError(msg, 400)
     } else {
         next()
