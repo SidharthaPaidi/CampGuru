e2d058607f1914b4743fa4a0b346c6a610f3e8fc
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-03-27 16:11:01 +0530

    add the author for Authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -34,6 +34,7 @@ router.get('/new',isLoggedIn, (req, res) => {
 //creating campground route
 router.post('',isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
    const campground = new Campground(req.body.campground)
+   campground.author = req.user._id
     await campground.save()
     req.flash('success', 'Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
@@ -41,13 +42,22 @@ router.post('',isLoggedIn, validateCampground, catchAsync(async (req, res, next)
 
 //viewing campground show route
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews')
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author')
+    console.log(campground)
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show', { campground })
 }))
 
 //editing campground route
 router.get('/:id/edit',isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/edit', { campground })
 
 }))
