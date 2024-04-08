b9744448e7697cbc90e9dcaa5e6781f9e82a2307
Sidhartha Paidi <paidisaisidhartha9@gmail.com>
2024-04-08 23:19:28 +0530

    Add 4 styles of map and style the map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,13 +1,43 @@
 
 
-mapboxgl.accessToken = mapToken; // Set the Mapbox access token 
-const map = new mapboxgl.Map({ // Create a new Mapbox map 
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9, // starting zoom
-});
+// mapboxgl.accessToken = mapToken; // Set the Mapbox access token 
+// const map = new mapboxgl.Map({ // Create a new Mapbox map 
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//     center: campground.geometry.coordinates, // starting position [lng, lat]
+//     zoom: 9, // starting zoom
+// });
 
+
+// // Add a marker to the map 
+// new mapboxgl.Marker()
+//     .setLngLat(campground.geometry.coordinates)
+//     .setPopup(
+//         new mapboxgl.Popup({ offset: 25 })
+//             .setHTML(
+//                 `<h3>${campground.title}</h3><p>${campground.location}</p>`
+//             )
+//     )
+//     .addTo(map)
+
+    mapboxgl.accessToken = mapToken; // Set the Mapbox access token 
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
+        center: campground.geometry.coordinates, // starting position [lng, lat]
+        zoom: 13 // starting zoom
+    });
+
+    const layerList = document.getElementById('menu');
+    const inputs = layerList.getElementsByTagName('input');
+
+    for (const input of inputs) {
+        input.onclick = (layer) => {
+            const layerId = layer.target.id;
+            map.setStyle('mapbox://styles/mapbox/' + layerId);
+        };
+    }
 
 // Add a marker to the map 
 new mapboxgl.Marker()
@@ -19,3 +49,4 @@ new mapboxgl.Marker()
             )
     )
     .addTo(map)
+
